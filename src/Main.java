eae623599a0fc865eb2824bcd0007b3b9ad9b6b8
Poviraevsergei <pacmanmozg1441>
2022-06-21 10:41:57 +0300

    cherry1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("My second commit from main");
         System.out.println("My second commit from main111111");
         System.out.println("Code from second Branch!");
+        System.out.println("CHERRY1");
     }
 }
