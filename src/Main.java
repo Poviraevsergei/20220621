60ada4589db543dd0da02c3dc5375f049e72c6a6
Poviraevsergei <pacmanmozg1441>
2022-06-21 10:42:11 +0300

    cherry123

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Code from second Branch!");
         System.out.println("CHERRY1");
         System.out.println("CHERRY12");
+        System.out.println("CHERRY123");
     }
 }
