ece3de6ef3af82135b4ad1ce54051977dfac4d16
Poviraevsergei <pacmanmozg1441>
2022-06-21 10:23:30 +0300

    my second main 11111

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first commit");
         System.out.println("My second commit from main");
+        System.out.println("My second commit from main111111");
     }
 }
